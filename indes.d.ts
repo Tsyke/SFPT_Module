bfd9a153ccc66acbf1c56a7964cfb0daf76d1702
unknown <weedpassif@gmail.com>
2021-09-19 22:31:14 +0200

    first commit

---
+++ indes.d.ts
@@ -0,0 +1,10 @@
+import { Client, MessageAttachment, MessageEmbed, Message, ColorResolvable, Snowflake } from "discord.js";
+
+declare module "SFPT";
+
+
+export function tokenGen(Length: number): string;
+export function idGen(Length: number): string;
+export function captchaCodeGen(Lenght: number): string;
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+const tokenGen = require('./functions/tokenGen'),
+    idGen = require('./functions/idGen'),
+    captchaCodeGen = require('./functions/captchaCodeGen')
+
+
+module.exports = {
+    tokenGen,
+    idGen,
+    captchaCodeGen
+};
